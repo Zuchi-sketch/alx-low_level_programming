506e217be41b69d70310ccb71b5cf7a7288ff80d
Collins <collinszurum@gmail.com>
2022-04-13 02:25:22 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,13 +8,11 @@
 void print_times_table(int n)
 {
 	int a, b, op;
-	
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
 		{
 			_putchar(48);
-			
 			for (b = 1; b <= n; b++)
 			{
 				op = a * b;
