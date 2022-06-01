750eb932f8a7900549fd342fc3ebdbd9c31eeb2e
Collins <collinszurum@gmail.com>
2022-06-01 07:47:53 -0700

    textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it to the POSIX
+ * standard output.
  * @filename: file.
  * @letters: Number of letters it should read and print.
  * Return: Actual number of letters it could read and print.
